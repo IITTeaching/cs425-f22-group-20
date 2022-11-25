5eeb04217f8ade17ae787809427d145c4c0a2079
marellec <leonmarelle0@gmail.com>
2022-11-25 13:45:57 -0600

    added query_to_value() function

--- db_execution.py
+++ db_execution.py
@@ -1,3 +1,4 @@
+from typing import Any
 import sqlalchemy as db # type: ignore
 import pandas as pd # type: ignore 
 
@@ -39,12 +40,32 @@ class DBExecuter:
         try:
             ResultSet = ResultProxy.fetchall()
         except db.exc.ResourceClosedError as e:
-            raise ValueError("This query doesn't return a table, "
-                             "so it can't be converted to a dataframe. \n"
-                             "Only call query_to_df() with a `SELECT` statement, \n"
-                             "or a statement with a `RETURNING` clause \n"
-                             "statement. \n"
-                             "Call run_query() to just update the database.") from e
+            raise ValueError(
+                "This query doesn't return a table, \n  "
+                "so it can't be converted to a dataframe. \n  "
+                "Only call query_to_df() with a `SELECT` statement, \n  "
+                "or a statement with a `RETURNING` clause. \n  "
+                "Call run_query() to just update the database."
+            ) from e
+        
+    def query_to_value(self, query: str) -> Any:
+        """Turns a SQL query (that returns a table with 1 value) into its single value"""
+        df = self.query_to_df(query)
+        if (len(df.columns) == 1 and len(df[df.columns[0]]) == 1):
+            # return single value
+            return df[df.columns[0]][0]
+        elif (len(df.columns) == 0 or len(df[df.columns[0]]) == 0):
+            raise ValueError(
+                "This query returns an empty table, \n  "
+                "so there is no value to return. \n  "
+            )
+        else:
+            raise ValueError(
+                "This query returns a table that has more than one value, \n  "
+                "so it should be converted to a dataframe to be manipulated/viewed. \n  "
+                "Only call query_to_value() when you expect 1 value. \n  "
+                "Call query_to_df() to convert to a dataframe."
+            )
             
     def run_query(self, query: str) -> None:
         """Runs a SQL query (that updates the database without returning a table)"""
@@ -53,8 +74,10 @@ class DBExecuter:
         
         # if there is a resulting table
         if (ResultProxy.returns_rows):
-            raise ValueError("This query returns a table, "
-                             "so it should be converted to a dataframe to be viewed. \n"
-                             "Only call run_query() with non-SELECT statements. \n"
-                             "Call query_to_df() to convert to a dataframe.")
+            raise ValueError(
+                "This query returns a table, \n  "
+                "so it should be converted to a dataframe to be viewed. \n  "
+                "Only call run_query() with non-SELECT and non-RETURNING statements. \n  "
+                "Call query_to_df() to convert to a dataframe."
+            )
         
